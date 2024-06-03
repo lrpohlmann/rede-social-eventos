51b6718251cdc617eac5a66992fb8b9ebd8ea0b0
leonardo <leo.pohlmann@hotmail.com>
2024-06-03 14:18:37 -0300

    redirecionamento para ond-e-hj

--- ond_eventos/views/home.py
+++ ond_eventos/views/home.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 
 
 def home_view(request):
-    return render(request, "pagina/home.html")
+    return redirect("pergunta:ond-e-hj")
